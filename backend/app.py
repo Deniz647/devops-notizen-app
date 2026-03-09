5f29f4c6ea1bba2f2473f128431bc8c92c7cb8d2
Deniz Can <deniz-can96@hotmail.com>
2026-03-09 20:28:55 +0100

    Backend erste Version mit Flask

---
+++ backend/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+notes = []
+
+@app.route("/")
+def start():
+    return "Notizen Backend läuft"
+
+@app.route("/notes", methods=["GET"])
+def get_notes():
+    return {"notes": notes}
+
+@app.route("/add", methods=["POST"])
+def add_note():
+    data = request.json
+    note = data["note"]
+    notes.append(note)
+    return {"msg": "Notiz gespeichert"}
+
+@app.route("/delete/<id>", methods=["DELETE"])
+def delete_note(id):
+    notes.pop(int(id))
+    return {"msg": "Notiz gelöscht"}
+
+app.run(host="0.0.0.0", port=3000)
