b6d562158a97b25b47fcf90f51c5a3d3e8a45f80
Deniz Can <deniz-can96@hotmail.com>
2026-03-10 18:40:56 +0100

    notizen speichern jetzt auch nach neustart

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,25 @@
 from flask import Flask, request
+from flask_cors import CORS
+import json
+import os
 
 app = Flask(__name__)
 
-notes = []
+CORS(app)
+
+DATEI_NAME = "notes.json"
+
+def load_notes():
+    if os.path.exists(DATEI_NAME):
+        with open(DATEI_NAME, "r", encoding="utf-8") as file:
+            return json.load(file)
+    return []
+
+def save_notes():
+    with open(DATEI_NAME, "w", encoding="utf-8") as file:
+        json.dump(notes, file, ensure_ascii=False, indent=2)
+
+notes = load_notes()
 
 @app.route("/")
 def start():
@@ -21,11 +38,13 @@ def add_note():
 
     note = data["note"]
     notes.append(note)
+    save_notes()
     return {"msg": "Notiz gespeichert"}
 
 @app.route("/delete/<id>", methods=["DELETE"])
 def delete_note(id):
     notes.pop(int(id))
+    save_notes()
     return {"msg": "Notiz gelöscht"}
 
 @app.route("/test")
